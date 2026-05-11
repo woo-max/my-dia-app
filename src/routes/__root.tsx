a734bdbd6ea633f73f0c6904224ad010b02a3e83
woo-max <why5779@gmail.com>
2026-05-12 06:56:32 +0900

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,15 +13,14 @@ function RootComponent() {
   useEffect(() => {
     const setupBackButton = async () => {
       const backHandler = await CapApp.addListener('backButton', () => {
-        // [심사위원 포인트] 해시(#modal) 존재 여부와 DOM 상태를 동시에 체크하여 가장 확실한 분기 처리
-        const hasHash = window.location.hash.includes('modal');
-        const hasOpenDialog = !!document.querySelector('[role="dialog"]') || !!document.querySelector('.fixed.inset-0');
+        // 해시나 특정 팝업 클래스가 있으면 뒤로가기만 수행
+        const hasPopup = window.location.hash.includes('modal') || 
+                         !!document.querySelector('.fixed') || 
+                         !!document.querySelector('[role="dialog"]');
 
-        if (hasHash || hasOpenDialog) {
-          // 팝업이 하나라도 감지되면 히스토리만 뒤로 보내 앱 종료를 막음
+        if (hasPopup) {
           window.history.back();
         } else if (window.location.pathname === '/') {
-          // 메인 화면이고 팝업이 전혀 없을 때만 앱 종료
           CapApp.exitApp();
         } else {
           window.history.back();
